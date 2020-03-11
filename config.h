8a5db17d978ced003b1becfd8323498cd5587dfd
brambijnens <bram@bijnens.com>
2020-03-11 21:34:05 +0100

    changing colorscheme

--- config.h
+++ config.h
@@ -8,13 +8,23 @@ static const char *fonts[] = {
 	"monospace:size=16"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char col_black[]		= "#282a2e"; /* black   */
+static const char col_red[]			= "#a54242"; /* red     */
+static const char col_green[]		= "#8c9440"; /* green   */
+static const char col_yellow[]		= "#de935f"; /* yellow  */
+static const char col_blue[]		= "#5f819d"; /* blue    */
+static const char col_magenta[]		= "#85678f"; /* magenta */
+static const char col_cyan[]		= "#5e8d87"; /* cyan    */
+static const char col_white[]		= "#707880"; /* white   */
+static const char col_bg[]			= "#1d1f21"; /* background */
+static const char col_fg[]			= "#c5c8c6"; /* foreground */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#EBDBB2", "#000000" },
-	[SchemeSel] = { "#000000", "#EBDBB2" },
-	[SchemeSelHighlight] = { "#9F1812", "#EBDBB2" },
-	[SchemeNormHighlight] = { "#FB4934", "#000000" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*							fg				bg       */
+	[SchemeNorm]			= {	col_blue,		col_black },
+	[SchemeSel]				= {	col_bg,			col_blue },
+	[SchemeSelHighlight]	= { col_red,		col_blue },
+	[SchemeNormHighlight]	= { col_red,		col_bg },
+	[SchemeOut]				= { col_black,		col_cyan },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
